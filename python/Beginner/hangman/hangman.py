9895ce6a9aaf0ca1a7288bb140c9fb8b787b82c4
Jack <jackmcardle2011@hotmail.co.uk>
2022-05-05 22:23:01 +0100

    changes made to hangman, removed oop

--- python/Beginner/hangman/hangman.py
+++ python/Beginner/hangman/hangman.py
@@ -3,41 +3,29 @@
 import random
 ran_int = random.randint(0,4) #generating a random int for word seed
 words = ['elephant', 'table', 'uniform', 'danger', 'invite'] #words that the game will choose from
+current_word = words[ran_int] #assigning value to word from array via ran num seed
+max_guesses = 6
+guess_count = 0   
+hidden_word = ''
 
-def set_current_word():
-    current_word = words[ran_int] #assigning value to word from array via ran num seed
-    print(current_word)
-    return current_word
+for letters in range(len(current_word)): #length of hidden word
+    hidden_word += '_' #creating a hidden word same length as current word
+print(hidden_word)
+print(current_word)
 
-current_word = set_current_word() #assgining word
-
-def start():
-    global guess_count
-    guess_count = 0
-    set_current_word() 
-    while guess_count <= 6:
-        guessed_char = input("Please guess a single letter. ")
-        guess_letter(guessed_char)
-
-def guess_letter(guessed_char):
-    print(current_word)
-    
+while guess_count <= 6:
+    guessed_char = input("Please guess a single letter. ")
     if guessed_char in current_word:
         print("Nice! That letter was in the word.")
+        for letter in range(len(current_word)):   
+            if current_word[letter] == guessed_char:
+                hidden_word = hidden_word[:letter] + guessed_char + hidden_word[letter + 1:]      #strings are immutable in python!! remember this!!!!!
+                print(hidden_word)              
     else:
         print("That was not in the word!")
+    guess_count +=1
 
 
-def rules():
-    print("Here are the rules")
+print(hidden_word)
 
-def menu():
-    user_choice = input("(1)Play\n(2)Rules\n(3)Quit\n")
-    if user_choice == "1":
-        start()
-    elif user_choice == "2":
-        rules()
-    else:
-        print("Thanks for playing!") 
 
-menu()
